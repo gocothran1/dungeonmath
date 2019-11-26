591eb1460bf29e1f84c1100bb0ccf4a776a2b0b5
George Cothran <gocothran1@gmail.com>
2019-11-26 15:10:45 -0500

    Barebones functional charachter sheet; input not sanitized; classes not in separate files; no randomizer

--- src/App.js
+++ src/App.js
@@ -2,25 +2,70 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props); {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+      <h1>WELCOME TO THE DnD CHARACTER CREATOR</h1>
+      <h3>Use this tool to enter a number and get your desired traits + modifier</h3>      
+      <div className = "container">
+      <BoxDuo name = "Strength"/>
+      <BoxDuo name = "Dexterity"/>
+      <BoxDuo name = "Constitution"/>
+      <BoxDuo name = "Intelligence"/>
+      <BoxDuo name = "Wisdom"/>
+      <BoxDuo name = "Charisima"/>
+      </div>
+      </div>
+    );
+  }
 }
 
+class BoxDuo extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      modifier: 0,
+      name: this.props.name
+    };
+  }
+  calculateModifier(stat){
+    return Math.floor((stat-10)/2)
+
+  }
+  changeText = (event) =>{
+    this.setState({
+      modifier: this.calculateModifier(event.target.value)
+    })
+  }
+
+  dieRoll(event) {
+    return(
+      Math.floor(Math.random()*15)+3
+    );
+  }
+render(){
+  return(
+    <div className="boxlayout">
+      <h3 className = "title">{this.state.name}</h3>
+      <textarea className="statfield" onChange={this.changeText}>
+
+      </textarea>
+      <div className="modfield">
+        {this.state.modifier}
+      </div>
+    </div>
+  )
+}
+
+}
+
+
 export default App;
+
